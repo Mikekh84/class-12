8704b44e3ec19915c5afec7dcce1a2239d6ffa16
Mike <mikekh@uw.edu>
2015-12-17 16:21:56 -0800

    working routing

--- scripts/articlesController.js
+++ scripts/articlesController.js
@@ -3,3 +3,10 @@ var articlesController = {};
 articlesController.index = function() {
   Article.loadAll(articleView.index);
 };
+
+articlesController.category = function (ctx, data) {
+  var categorydata = function(data) {
+    console.log(data);
+  }
+
+};
--- scripts/router.js
+++ scripts/router.js
@@ -2,4 +2,5 @@ page('/', articlesController.index);
 
 page('/about', reposController.index);
 
+page('/catergory/:catergory', articlesController.category);
 page.start();
